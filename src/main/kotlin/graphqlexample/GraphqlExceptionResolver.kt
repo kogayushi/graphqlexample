95f7ff53fde896bb319afdf77e796741ff4a148e
koga yushi <youshe.old.pleasure@gmail.com>
2022-10-04 09:24:31 +0900

    handle ConstraintViolationException for bean validation

--- src/main/kotlin/graphqlexample/GraphqlExceptionResolver.kt
+++ src/main/kotlin/graphqlexample/GraphqlExceptionResolver.kt
@@ -7,16 +7,29 @@ import graphqlexample.domain.model.NotFoundException
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter
 import org.springframework.graphql.execution.ErrorType
 import org.springframework.stereotype.Component
+import javax.validation.ConstraintViolationException
 
 @Component
 class GraphqlExceptionResolver : DataFetcherExceptionResolverAdapter() {
 
     override fun resolveToSingleError(ex: Throwable, env: DataFetchingEnvironment): GraphQLError? {
 
+        return super.resolveToSingleError(ex, env)
+    }
+
+    override fun resolveToMultipleErrors(ex: Throwable, env: DataFetchingEnvironment): List<GraphQLError>? {
+
         if (ex is NotFoundException) {
-            return GraphqlErrorBuilder.newError().errorType(ErrorType.NOT_FOUND).message(ex.message).build()
+            return listOf(GraphqlErrorBuilder.newError().errorType(ErrorType.NOT_FOUND).message(ex.message).build())
+        } else if(ex is ConstraintViolationException) {
+            return ex.constraintViolations.map {
+                GraphqlErrorBuilder.newError().errorType(ErrorType.BAD_REQUEST).message(it.message)
+                    .path(listOf(it.propertyPath.toString())) // パスはtoStringで得るしかないらしい
+                    .build()
+            }
         }
 
-        return super.resolveToSingleError(ex, env)
+
+        return super.resolveToMultipleErrors(ex, env)
     }
 }
