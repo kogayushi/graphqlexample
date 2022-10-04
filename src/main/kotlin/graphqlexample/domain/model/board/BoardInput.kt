9761ccd223594f459117a9dc79f827d458c20f2d
koga yushi <youshe.old.pleasure@gmail.com>
2022-10-04 09:24:56 +0900

    implement mutation

---
+++ src/main/kotlin/graphqlexample/domain/model/board/BoardInput.kt
@@ -0,0 +1,18 @@
+package graphqlexample.domain.model.board
+
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Size
+
+data class BoardInput(
+    @field: Size(max = 10)
+    @field: NotEmpty
+    var title: String?,
+    @field: NotEmpty
+    var detail: String?,
+    @field: NotEmpty
+    var hoge: String?,
+) {
+
+    val titleAsNonnull by lazy { this.title!! }
+    val detailAsNonnull by lazy { this.detail!! }
+}
--- src/main/kotlin/graphqlexample/presentation/graphql/BoardController.kt
+++ src/main/kotlin/graphqlexample/presentation/graphql/BoardController.kt
@@ -1,11 +1,16 @@
 package graphqlexample.presentation.graphql
 
 import graphqlexample.domain.model.board.Board
+import graphqlexample.domain.model.board.BoardInput
 import graphqlexample.domain.model.board.BoardRepository
+import graphqlexample.domain.model.user.UserRepository
 import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.Arguments
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 import java.util.*
+import javax.validation.Valid
 
 @Controller
 class BoardController(
@@ -17,4 +22,17 @@ class BoardController(
 
     @QueryMapping
     fun board(@Argument boardId: UUID): Board = boardRepository.resolveBy(boardId = boardId)
+
+    @MutationMapping
+    fun registerBoard(@Valid @Arguments boardInput: BoardInput): Board {
+
+        val boardId = UUID.randomUUID()
+        val authorId = UserRepository.USER_ID_1
+        return Board(
+            boardId = boardId,
+            title = boardInput.title ?: "anan",
+            detail = boardInput.detail ?: "enen",
+            authorId = authorId,
+        )
+    }
 }
