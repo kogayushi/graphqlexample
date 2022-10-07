7cf543ecce3574375a09679c682b1dfed064f7ce
koga yushi <youshe.old.pleasure@gmail.com>
2022-10-07 22:46:40 +0900

    implement graphql subscription

--- src/main/kotlin/graphqlexample/domain/model/board/BoardRepository.kt
+++ src/main/kotlin/graphqlexample/domain/model/board/BoardRepository.kt
@@ -2,18 +2,20 @@ package graphqlexample.domain.model.board
 
 import graphqlexample.domain.model.NotFoundException
 import graphqlexample.domain.model.user.UserRepository
+import graphqlexample.presentation.graphql.BoardHandler
 import org.springframework.stereotype.Component
 import java.util.UUID
 
 @Component
-class BoardRepository {
-
+class BoardRepository(
+    private val boardHandler: BoardHandler,
+) {
     companion object {
         val BOARD_ID_1 = UUID.fromString("098cb97b-3816-4342-817a-9c54656cf0a2")
         val BOARD_ID_2 = UUID.fromString("1be9194e-91de-44b9-9e77-81fc8c144d69")
     }
 
-    private val boards = listOf(
+    private val boards = mutableListOf(
         Board(
             boardId = BOARD_ID_1,
             title = "タイトル1",
@@ -30,4 +32,9 @@ class BoardRepository {
     fun resolveAll(): List<Board> = boards
 
     fun resolveBy(boardId: UUID): Board = this.boards.find { it.boardId == boardId } ?: throw NotFoundException("board not found => $boardId")
+
+    fun save(board: Board) {
+        boards += board
+        boardHandler.addBoard(board)
+    }
 }
